f432b4b0b165a207fd45842410d0a4b1f73329f9
UnicornDad8 <unicorndad8@gmail.com>
2023-07-10 19:08:25 -0400

    Added FoodItem to BasketScreen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { View } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { selectRestaurant } from "../features/restaurantSlice";
-import { selectBasketItems } from "../features/basketSlice";
+import { selectBasketItems, removeFromBasket } from "../features/basketSlice";
+import Currency from "react-currency-formatter";
 import { useNavigation } from "@react-navigation/native";
 import { XMarkIcon } from "react-native-heroicons/solid";
+import { urlFor } from "../sanity";
 import styled from "styled-components/native";
 
 const BasketScreen = () => {
@@ -16,7 +18,7 @@ const BasketScreen = () => {
 
   useEffect(() => {
     const groupedItems = items.reduce((results, item) => {
-      (results[items.id] = results[items.id] || []).push(item);
+      (results[item.id] = results[item.id] || []).push(item);
       return results;
     }, {});
 
@@ -25,7 +27,7 @@ const BasketScreen = () => {
 
   return (
     <BasketScreenContainer>
-      <HeaderOutter>
+      <BasketWrapper>
         <ModalHeader>
           <View>
             <BasketName>Basket</BasketName>
@@ -35,7 +37,40 @@ const BasketScreen = () => {
             <XMarkIcon size={20} color="#fff" />
           </BackButton>
         </ModalHeader>
-      </HeaderOutter>
+
+        <DeliverContainer>
+          <UserImage
+            source={{
+              uri: "https://github.com/Ceci007/image-repository/blob/master/img/team-1.jpg?raw=true",
+            }}
+          />
+          <DeliverTime>Deliver in 50 - 75 min</DeliverTime>
+          <TouchableOpacity>
+            <ChangeText>Change</ChangeText>
+          </TouchableOpacity>
+        </DeliverContainer>
+
+        <ScrollView>
+          {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+            <FoodItem key={key}>
+              <Count>{items.length} x</Count>
+              <FoodImage
+                source={{ uri: urlFor(items[0]?.image).width(80).url() }}
+              />
+              <Name>{items[0]?.name}</Name>
+              <Price>
+                <Currency quantity={items[0]?.price} currency="GBP" />
+              </Price>
+
+              <TouchableOpacity
+                onPress={() => dispatch(removeFromBasket({ id: key }))}
+              >
+                <RemoveText>Remove</RemoveText>
+              </TouchableOpacity>
+            </FoodItem>
+          ))}
+        </ScrollView>
+      </BasketWrapper>
     </BasketScreenContainer>
   );
 };
@@ -47,7 +82,7 @@ const BasketScreenContainer = styled.SafeAreaView`
   background: #fff;
 `;
 
-const HeaderOutter = styled.View`
+const BasketWrapper = styled.View`
   flex: 1;
   background: #f1f1f1;
 `;
@@ -68,10 +103,11 @@ const ModalHeader = styled.View`
   position: relative;
   padding: 25px;
   border-bottom-width: 1px;
-  border-bottom-color: #00ccbb;
+  border-bottom-color: #dcdad7;
   background: #fff;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 `;
+
 const BackButton = styled.TouchableOpacity`
   position: absolute;
   top: 20px;
@@ -82,4 +118,63 @@ const BackButton = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
   border-radius: 15px;
+`;
+
+const DeliverContainer = styled.View`
+  flex-direction: row;
+  padding: 15px 20px;
+  background: #fff;
+  margin: 25px 0;
+  align-items: center;
+`;
+
+const UserImage = styled.Image`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  margin-right: 10px;
+`;
+
+const DeliverTime = styled.Text`
+  flex: 1;
+`;
+
+const ChangeText = styled.Text`
+  color: #00ccbb;
+`;
+
+const FoodItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 25px;
+  background: #fff;
+  border-bottom-width: 1px;
+  border-bottom-color: #dcdad7;
+`;
+
+const Count = styled.Text`
+  color: #00ccbb;
+`;
+
+const FoodImage = styled.Image`
+  height: 60px;
+  width: 60px;
+  border-radius: 30px;
+  margin: 0 10px;
+`;
+
+const Name = styled.Text`
+  flex: 1;
+  font-weight: 500;
+`;
+
+const Price = styled.Text`
+  color: gray;
+  opacity: 0.6;
+`;
+
+const RemoveText = styled.Text`
+  color: #00ccbb;
+  font-size: 14px;
+  margin-left: 10px;
 `;
