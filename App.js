f75c45873492a6486ea2801755d4b74334895d84
UnicornDad8 <unicorndad8@gmail.com>
2023-07-10 14:04:32 -0400

    added redux and basketReducer

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import RestaurantScreen from "./screens/RestaurantScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { setupURLPolyfill } from "react-native-url-polyfill";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 setupURLPolyfill();
 const Stack = createNativeStackNavigator();
@@ -10,10 +12,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
--- components/DishRow.js
+++ components/DishRow.js
@@ -1,38 +1,96 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { TouchableOpacity } from "react-native";
 import Currency from "react-currency-formatter";
 import { urlFor } from "../sanity";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItemsWithId,
+} from "../features/basketSlice";
+import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
 import styled from "styled-components/native";
 
 function DishRow({ id, name, description, price, image }) {
+  const [isPressed, setIsPressed] = useState(false);
+  const dispatch = useDispatch();
+  const items = useSelector((state) => selectBasketItemsWithId(state, id));
+
+  const addItemToBasket = () => {
+    dispatch(
+      addToBasket({
+        id,
+        name,
+        description,
+        price,
+        image,
+      })
+    );
+  };
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+
+    dispatch(removeFromBasket({ id }));
+  };
+
   return (
-    <DishContainer
-      style={{
-        borderBottomColor: "#dcdad7",
-        borderBottomWidth: 1,
-        borderTopColor: "#dcdad7",
-        borderTopWidth: 1,
-      }}
-    >
-      <DishTop>
-        <DescriptionWrapper>
-          <Name>{name}</Name>
-          <Description>{description}</Description>
-          <CurrencyContainer>
-            <Currency quantity={price} currency="GBP" />
-          </CurrencyContainer>
-        </DescriptionWrapper>
-        <ImageContainer>
-          <Image
-            style={{
-              borderWidth: 1,
-              borderColor: "#F3F3F4",
-            }}
-            source={{ uri: urlFor(image).url() }}
-          />
-        </ImageContainer>
-      </DishTop>
-    </DishContainer>
+    <>
+      <DishContainer
+        style={
+          isPressed
+            ? { borderBottomWidth: 0 }
+            : {
+                borderBottomColor: "#dcdad7",
+                borderBottomWidth: 1,
+                borderTopColor: "#dcdad7",
+                borderTopWidth: 1,
+              }
+        }
+        onPress={() => setIsPressed(!isPressed)}
+      >
+        <DishTop>
+          <DescriptionWrapper>
+            <Name>{name}</Name>
+            <Description>{description}</Description>
+            <CurrencyContainer>
+              <Currency quantity={price} currency="GBP" />
+            </CurrencyContainer>
+          </DescriptionWrapper>
+          <ImageContainer>
+            <Image
+              style={{
+                borderWidth: 1,
+                borderColor: "#F3F3F4",
+              }}
+              source={{ uri: urlFor(image).width(100).url() }}
+            />
+          </ImageContainer>
+        </DishTop>
+      </DishContainer>
+
+      {isPressed && (
+        <DishBottom>
+          <QuantityContainer>
+            <IconsBox>
+              <TouchableOpacity onPress={removeItemFromBasket}>
+                <MinusCircleIcon
+                  color={items.length > 0 ? "#00CCBB" : "#dcdad7"}
+                  size={40}
+                />
+              </TouchableOpacity>
+              <Quantity>{items.length}</Quantity>
+              <TouchableOpacity onPress={addItemToBasket}>
+                <PlusCircleIcon
+                  color={items.length > 0 ? "#00CCBB" : "#dcdad7"}
+                  size={40}
+                />
+              </TouchableOpacity>
+            </IconsBox>
+          </QuantityContainer>
+        </DishBottom>
+      )}
+    </>
   );
 }
 
@@ -49,6 +107,7 @@ const DishTop = styled.View`
 
 const Name = styled.Text`
   font-size: 22px;
+  font-weight: 600;
   margin-bottom: 5px;
 `;
 
@@ -78,4 +137,22 @@ const Image = styled.Image`
   width: 100%;
   height: 100%;
   object-fit: cover;
+`;
+
+const DishBottom = styled.View`
+  background: #fff;
+  padding: 0 15px;
+  padding-bottom: 10px;
+`;
+
+const QuantityContainer = styled.View``;
+
+const IconsBox = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Quantity = styled.Text`
+  font-weight: 700;
+  margin: 0 10px;
 `;
